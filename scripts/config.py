d002d4421e128e4b254fdeea9301bda970347750
Andreas Schmid <andreas.schmid@ur.de>
2022-02-19 22:20:18 +0100

    add constants for MIF extraction to config

--- scripts/config.py
+++ scripts/config.py
@@ -2,3 +2,7 @@ VIDEO_PATH = '../input_data/subsample'
 VIDEO_OUTPUT_PATH = '../videos_processed'
 VIDEO_FPS = 25
 KEEP_AUDIO = False
+MODEL_PATH = '../models/weights/resnet50_moments-fd0c4436.pth'
+CUDA_ON = False
+LABEL_PATH = '../models/labels/category_momentsv1.txt'
+RESULT_PATH = '../data'
