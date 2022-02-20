450053c7c44b3f9cc771053f92a401ee17ae2a8d
Andreas Schmid <andreas.schmid@ur.de>
2022-02-20 11:23:30 +0100

    add comments, comment our functions that aren't needed currently

--- scripts/VideoHandler.py
+++ scripts/VideoHandler.py
@@ -11,9 +11,11 @@ class VideoHandler():
         self.set_category(category)
         self.set_name(name)
 
+        # get list of csv files for the video
         paths = sorted(glob(f'{self.result_path}/{category}/{name}_frame_*.csv'))
         self._frame_count = len(paths)
 
+        # create data frame for the video by concatenating data for all frames
         self.data = pd.concat([pd.read_csv(f) for f in paths])
         self.set_file_id(file_id)
 
@@ -62,9 +64,9 @@ class VideoHandler():
     #def add_video(self, path):
     #    pass
 
-    # saves stored data in the desired format
-    def save(self, file_format='csv'):
-        pass
+    ## saves stored data in the desired format
+    #def save(self, file_format='csv'):
+    #    pass
 
     # combine redundant actions to new_action (e.g. singing_child and singing_adult to singing)
     # default method: average
@@ -77,10 +79,13 @@ class VideoHandler():
             self.data = self.data.append({'frame' : frame, 'action' : new_action, 'probability' : np.mean(values), 'file' : self.file_id}, ignore_index=True)
         self.data = self.data[self.data.action.isin(actions) == False]
 
-    def generate_matrix(self, method, file_id_1, frame_id_1, file_id_2, frame_id_2, categories=[]):
-        # not sure what it's comparing - better ask the experts
-        pass
+    ## might be better somewhere else
+    #def generate_matrix(self, method, file_id_1, frame_id_1, file_id_2, frame_id_2, categories=[]):
+    #    # not sure what it's comparing - better ask the experts
+    #    pass
 
+    # returns slice of data set for one frame (equal to one CSV file)
+    # Todo: currently unused
     def get_frame_data(self, frame):
         # todo exception handling
         #  * frame not in number of frames
@@ -90,6 +95,7 @@ class VideoHandler():
         return self.data[self.data['frame'] == frame]
 
     # returns the most important frame for specified file
+    # Todo: split up into multiple private functions as more methods are added
     def get_mif(self, method=''):
         df = self.data[self.data['action'] == self._category]
         mif_row = df.loc[df['probability'] == df['probability'].max()]
