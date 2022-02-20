62a00f4ba039bfcc9ee84a239699b047214bcadc
Andreas Schmid <andreas.schmid@ur.de>
2022-02-20 12:14:12 +0100

    split up probability extraction and initializing the model into separate files

--- scripts/mif_extraction.py
+++ scripts/mif_extraction.py
@@ -1,67 +1,21 @@
 import os
-from pathlib import Path
-import numpy as np
 import pandas as pd
-import torch
-import torch.nn as nn
-import torchvision.models as models
-import torchvision.transforms as transforms
-from torch.autograd import Variable
-from torch.nn import functional as F
-from decord import VideoReader
 from decord import cpu, gpu
-import decord
+from decord import VideoReader
 
 from video_converter import convert_videos
 import config
 #from util import get_video_list
 
-decord.bridge.set_bridge('native')
+from model import calculate_probabilities
 
-cuda_on = config.CUDA_ON
-model_path = config.MODEL_PATH
-
-print('loading model...')
-resnet50 = models.resnet50(pretrained=False, progress=True, num_classes=339)
-
-if cuda_on:
-    resnet50 = resnet50.to('cuda')
-
-# Load pretrained weights (MiTv1)
-resnet50.load_state_dict(torch.load(model_path))
-
-# AB: source / structure
-# Evaluation mode
-resnet50.eval()
-
-transformation = transforms.Compose([
-                                     transforms.ToPILImage(mode='RGB'), # required if the input image is a nd.array
-                                     transforms.Resize(224), # To be changed to rescale to keep the aspect ration?
-                                     transforms.CenterCrop((224, 224)), # AB: cropped to 224x224px
-                                     transforms.ToTensor(), # AB: creates Tensor (?)
-                                     transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                                          std=[0.229, 0.224, 0.225])
-])
-
-
-label_path = config.LABEL_PATH
-
-def load_categories():
+def load_categories(label_path):
     """Load categories."""
     with open(label_path) as f:
         return [line.rstrip() for line in f.readlines()]
 
-print('loading categories...')
-# load categories
-categories = load_categories()
 
-print('converting videos...')
-
-video_list = convert_videos(input_path=config.VIDEO_PATH, output_path=config.VIDEO_OUTPUT_PATH, keep_audio=config.KEEP_AUDIO, video_fps=config.VIDEO_FPS, overwrite=False)
-
-counter = 0
-
-def calculate_probabilities(video, result_path=config.RESULT_PATH):
+def process_video(video, result_path=config.RESULT_PATH, process=calculate_probabilities):
     reader = VideoReader(video['path'])
     frames = reader.get_batch([i for i in range(len(reader))])
 
@@ -77,25 +31,33 @@ def calculate_probabilities(video, result_path=config.RESULT_PATH):
 
         print(f'frame {frame_index+1}/{len(reader)}')
         img = frames.asnumpy()[frame_index]
-        img_transformed = transformation(img)
-        if cuda_on:
-            img_transformed = img_transformed.to_cuda()
-        logit = resnet50.forward(img_transformed.unsqueeze(0))
-        h_x = F.softmax(logit, 1).data.squeeze()
-        probabilities, idx = h_x.sort(0, True)
+
+        probabilities, idx = process(img)
         for category_index in range(len(probabilities)):
             try:
                 if(len(probabilities.data.numpy()[category_index]) > 1):
                     print(probabilities.data.numpy())
             except:
+                # more than one probability for a category
                 pass
             df = df.append({'frame' : frame_index, 'action' : categories[idx[category_index]], 'probability' : probabilities.data.numpy()[category_index]}, ignore_index=True)
         df.to_csv(f'{result_path}/{video["category"]}/{video["name"]}_frame_{frame_index:03d}.csv', index=False)
 
 
-print('doing magic...')
+if __name__ == '__main__':
+    print('loading categories...')
+    # load categories
+    categories = load_categories(config.LABEL_PATH)
 
-for video in video_list:
-    print(f'{video["path"]} {counter+1}/{len(video_list)}')
-    calculate_probabilities(video, result_path=config.RESULT_PATH)
-    counter += 1
+    print('converting videos...')
+
+    video_list = convert_videos(input_path=config.VIDEO_PATH, output_path=config.VIDEO_OUTPUT_PATH, keep_audio=config.KEEP_AUDIO, video_fps=config.VIDEO_FPS, overwrite=False)
+
+    print('doing magic...')
+
+    counter = 0
+
+    for video in video_list:
+        print(f'{video["path"]} {counter+1}/{len(video_list)}')
+        process_video(video, result_path=config.RESULT_PATH, process=calculate_probabilities)
+        counter += 1
---
+++ scripts/model.py
@@ -0,0 +1,43 @@
+import torch
+import torchvision.models as models
+import torchvision.transforms as transforms
+from torch.nn import functional as F
+import decord
+
+import config
+
+decord.bridge.set_bridge('native')
+
+cuda_on = config.CUDA_ON
+model_path = config.MODEL_PATH
+
+print('loading model...')
+resnet50 = models.resnet50(pretrained=False, progress=True, num_classes=339)
+
+if cuda_on:
+    resnet50 = resnet50.to('cuda')
+
+# Load pretrained weights (MiTv1)
+resnet50.load_state_dict(torch.load(model_path))
+
+# AB: source / structure
+# Evaluation mode
+resnet50.eval()
+
+transformation = transforms.Compose([
+                                     transforms.ToPILImage(mode='RGB'), # required if the input image is a nd.array
+                                     transforms.Resize(224), # To be changed to rescale to keep the aspect ration?
+                                     transforms.CenterCrop((224, 224)), # AB: cropped to 224x224px
+                                     transforms.ToTensor(), # AB: creates Tensor (?)
+                                     transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                          std=[0.229, 0.224, 0.225])
+])
+
+def calculate_probabilities(img, model=resnet50):
+    img_transformed = transformation(img)
+    if cuda_on:
+        img_transformed = img_transformed.to_cuda()
+    logit = model.forward(img_transformed.unsqueeze(0))
+    h_x = F.softmax(logit, 1).data.squeeze()
+    probabilities, idx = h_x.sort(0, True)
+    return probabilities, idx
