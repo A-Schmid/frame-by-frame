f9a74837889b92b23e48740be0d85bf870478ba4
Andreas Schmid <andreas.schmid@ur.de>
2022-02-23 19:25:52 +0100

    results of processing are now saved as one csv per video instead of one csv per frame to decrease processing time

--- scripts/video_processing.py
+++ scripts/video_processing.py
@@ -12,19 +12,22 @@ import config
 
 from model import calculate_probabilities, load_categories
 
+from time import perf_counter
+
 def process_video(video, result_path=config.RESULT_PATH, process=calculate_probabilities):
     reader = VideoReader(video['path'])
     frames = reader.get_batch([i for i in range(len(reader))])
 
-    for frame_index in range(len(reader)):
-        if os.path.exists(f'{result_path}/{video["category"]}/{video["name"]}_frame_{frame_index:03d}.csv'):
-            # skip if data already exists
-            # Todo: overwrite flag
-            continue
-        elif not os.path.exists(f'{result_path}/{video["category"]}'):
-            os.makedirs(f'{result_path}/{video["category"]}')
+    frame_data = []
 
-        df = pd.DataFrame(columns=['frame', 'action', 'probability'])
+    if os.path.exists(f'{result_path}/{video["category"]}/{video["name"]}.csv'):
+        # skip if data already exists
+        # Todo: overwrite flag
+        return False
+    elif not os.path.exists(f'{result_path}/{video["category"]}'):
+        os.makedirs(f'{result_path}/{video["category"]}')
+
+    for frame_index in range(len(reader)):
 
         print(f'frame {frame_index+1}/{len(reader)}')
         img = frames.asnumpy()[frame_index]
@@ -37,18 +40,28 @@ def process_video(video, result_path=config.RESULT_PATH, process=calculate_proba
             except:
                 # more than one probability for a category
                 pass
-            df = df.append({'frame' : frame_index, 'action' : categories[idx[category_index]], 'probability' : probabilities.data.numpy()[category_index]}, ignore_index=True)
-        df.to_csv(f'{result_path}/{video["category"]}/{video["name"]}_frame_{frame_index:03d}.csv', index=False)
+            frame_data.append({'frame' : frame_index, 'action' : categories[idx[category_index]], 'probability' : probabilities.data.numpy()[category_index]})
 
+    #print(frame_data)
+    df = pd.DataFrame(frame_data)
+    #print(df)
+    df.to_csv(f'{result_path}/{video["category"]}/{video["name"]}.csv', index=False)
+    return True
 
 if __name__ == '__main__':
     print('loading categories...')
     # load categories
     categories = load_categories(config.LABEL_PATH)
 
-    print('converting videos...')
+    data_path = config.DATA_PATH
 
-    video_list = convert_videos(input_path=config.VIDEO_PATH, output_path=config.VIDEO_OUTPUT_PATH, keep_audio=config.KEEP_AUDIO, video_fps=config.VIDEO_FPS, overwrite=False)
+    # todo support args
+    path_videos_converted = f'{data_path}/videos_converted.csv'
+
+    #video_list = convert_videos(input_path=config.VIDEO_PATH, output_path=config.VIDEO_OUTPUT_PATH, keep_audio=config.KEEP_AUDIO, video_fps=config.VIDEO_FPS, overwrite=False)
+
+    df_videos = pd.read_csv(path_videos_converted)
+    video_list = df_videos.to_dict('records')
 
     print('doing magic...')
 
@@ -56,5 +69,9 @@ if __name__ == '__main__':
 
     for video in video_list:
         print(f'{video["path"]} {counter+1}/{len(video_list)}')
-        process_video(video, result_path=config.RESULT_PATH, process=calculate_probabilities)
+        start = perf_counter()
+        b = process_video(video, result_path=config.RESULT_PATH, process=calculate_probabilities)
+        end = perf_counter()
+        if b == True:
+            print((end-start))
         counter += 1
