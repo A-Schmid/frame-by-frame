97e3ab039b67ee60b3d1c2be49e6a2f409b1e08d
Andreas Schmid <andreas.schmid@ur.de>
2022-02-26 18:20:54 +0100

    audio is kept by default when converting videos

--- scripts/config.py
+++ scripts/config.py
@@ -1,7 +1,7 @@
 VIDEO_PATH = '../input_data/complete'
 VIDEO_OUTPUT_PATH = '../videos_processed'
 VIDEO_FPS = 25
-KEEP_AUDIO = False
+KEEP_AUDIO = True
 MODEL_PATH = '../models/weights/resnet50_moments-fd0c4436.pth'
 CUDA_ON = False
 LABEL_PATH = '../models/labels/category_momentsv1.txt'
